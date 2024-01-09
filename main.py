da0acf505908efceb02e61cdad516159253791b5
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-01-09 15:07:54 +0100

    updated UI

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from tkinter import *
+from tkinter.ttk import *
 from tkinter import filedialog
 
 root = Tk()
-root.geometry("400x250")
+root.geometry("600x450")
 root.title("File Converter")
 
 frame = Frame(root)
 frame.pack()
 
-text = Text(frame, height= 20, width= 70)
+text = Label(frame, font=(25))
 
 def openFileDialog():
     file_path = filedialog.askopenfilename()
-    text.insert("1.0", file_path)
+    if (file_path): 
+        text.config(text="File Path: "+file_path)
+        continueButton.config(state= NORMAL)
 
-button = Button(frame, text = "Select File", command=openFileDialog)
-button.pack(pady= 50)
+selectFileButton = Button(frame, text = "Select File", command=openFileDialog)
+selectFileButton.pack()
 text.pack(expand= True)
+
+continueButton = Button(frame, text="Convert", state= DISABLED)
+continueButton.pack(pady=20)
 
 root.mainloop()
